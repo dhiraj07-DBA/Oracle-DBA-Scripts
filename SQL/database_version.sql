5de1566f16d29c2a71f733b0fe82506c63c8cb2e
Dhiraj kumar <assets603@gmail.com>
2026-06-07 16:05:32 +0530

    Create database_version.sql

---
+++ SQL/database_version.sql
@@ -0,0 +1,11 @@
+/*
+====================================================
+Script Name : database_version.sql
+Purpose     : Database Version
+Author      : Dheeraj Kumar
+Oracle Ver  : 19c
+Created On  : 07-Jun-2026
+====================================================
+*/
+SELECT banner
+FROM v$version;
